13b23428b69445fa5b0676a97b2aab562de5cc6a
Extrez <emilio.danielsm@gmail.com>
2019-06-08 19:21:11 -0500

    Initialize Login Server Abstraction

---
+++ src/Imgeneus.Login/ILoginServer.cs
@@ -0,0 +1,29 @@
+﻿using Imgeneus.Network.InternalServer;
+using Imgeneus.Network.Server;
+using System.Collections.Generic;
+
+namespace Imgeneus.Login
+{
+    public interface ILoginServer : IServer
+    {
+        /// <summary>
+        /// Gets a list of all connected worlds.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<WorldServerInfo> GetConnectedWolrds();
+
+        /// <summary>
+        /// Gets a connected client by his username.
+        /// </summary>
+        /// <param name="userID">The user id.</param>
+        /// <returns>The connected client.</returns>
+        LoginClient GetClientByUserID(int userID);
+
+        /// <summary>
+        /// Verify if a client is connected to the login server.
+        /// </summary>
+        /// <param name="userID">The user id.</param>
+        /// <returns>True if client is connected.</returns>
+        bool IsClientConnected(int userID);
+    }
+}
---
+++ src/Imgeneus.Network/Packets/PacketType.cs
@@ -0,0 +1,12 @@
+﻿namespace Imgeneus.Network.Packets
+{
+    public enum PacketType : ushort
+    {
+        // Login opcodes
+        LOGIN_TERMINATE = 0x010B,
+        LOGIN_HANDSHAKE = 0xA101,
+        LOGIN_REQUEST = 0xA102,
+        SERVER_LIST = 0xA201,
+        SELECT_SERVER = 0xA202,
+    }
+}
